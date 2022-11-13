405a8bf80ed33a5c13fd6d1dedb5112044c7b9aa
Vince Jerald Villamora <vincevillamora@gmail.com>
2022-11-13 14:39:31 +0800

    fix(processor): simplify code for future bots

--- fpi-bot-api-core/src/main/java/com/abavilla/fpi/bot/processor/TgMsgEvtPcsr.java
+++ fpi-bot-api-core/src/main/java/com/abavilla/fpi/bot/processor/TgMsgEvtPcsr.java
@@ -19,136 +19,70 @@
 package com.abavilla.fpi.bot.processor;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 
 import com.abavilla.fpi.bot.codec.TGUpdateEvtCodec;
 import com.abavilla.fpi.bot.entity.telegram.TelegramEvt;
 import com.abavilla.fpi.bot.repo.TgEvtRepo;
-import com.abavilla.fpi.fw.dto.impl.RespDto;
-import com.abavilla.fpi.fw.exceptions.ApiSvcEx;
 import com.abavilla.fpi.fw.util.DateUtil;
 import com.abavilla.fpi.load.ext.dto.QueryDto;
-import com.abavilla.fpi.load.ext.rest.LoadQueryApi;
-import com.abavilla.fpi.login.ext.dto.SessionDto;
-import com.abavilla.fpi.login.ext.dto.WebhookLoginDto;
-import com.abavilla.fpi.login.ext.rest.TrustedLoginApi;
 import com.abavilla.fpi.msgr.ext.dto.MsgrMsgReqDto;
 import com.abavilla.fpi.msgr.ext.rest.TelegramReqApi;
 import com.abavilla.fpi.telco.ext.enums.BotSource;
-import com.mongodb.ErrorCategory;
-import com.mongodb.MongoWriteException;
 import com.pengrad.telegrambot.model.Update;
-import io.netty.handler.codec.http.HttpResponseStatus;
 import io.quarkus.logging.Log;
 import io.quarkus.vertx.ConsumeEvent;
 import io.smallrye.mutiny.Uni;
-import org.apache.commons.lang3.StringUtils;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @ApplicationScoped
-public class TgMsgEvtPcsr {
-
-  @RestClient
-  TrustedLoginApi loginApi;
-
-  @RestClient
-  LoadQueryApi loadApi;
-
-  @RestClient
-  TelegramReqApi telegramApi;
-
-  @Inject
-  TgEvtRepo repo;
+public class TgMsgEvtPcsr extends EvtPcsr<Update, TelegramReqApi, TgEvtRepo, TelegramEvt> {
 
   @ConsumeEvent(value = "telegram-msg-evt", codec = TGUpdateEvtCodec.class)
   public Uni<Void> process(Update evt) {
     Log.info("Received telegram update event: " + evt);
-    var log = mapToEntity(evt);
-    if (StringUtils.isNotBlank(log.getContent())) {
-      return repo.persist(log).chain(savedLog -> {
-          Log.info("Logged to db: " + savedLog);
-          return telegramApi.toggleTyping(String.valueOf(savedLog.getSenderId())).chain(() -> {
-            var login = new WebhookLoginDto();
-            login.setUsername(String.valueOf(savedLog.getSenderId()));
-            login.setBotSource(BotSource.TELEGRAM.getValue());
-            Log.info("Authenticating user: " + login);
-            return loginApi.webhookAuthenticate(login)
-              // process load
-              .chain(session -> processLoadQuery(login, session, evt)
-                .onFailure().recoverWithUni(ex -> handleApiEx(evt, session.getResp().getUsername(), ex)))
-              // login failures/query exceptions
-              .onFailure().recoverWithUni(ex -> handleApiEx(evt, ex));
-          });
-        }
-      )
-      .onFailure(ex -> ex instanceof MongoWriteException wEx &&
-        wEx.getError().getCategory().equals(ErrorCategory.DUPLICATE_KEY))
-      .recoverWithItem(() -> {
-        Log.warn("Received duplicate updateId: %d, senderId: %d, messageId: %d"
-          .formatted(log.getUpdateId(), log.getSenderId(), log.getMessageId()));
-        return null;
-      })
-      .onFailure().invoke(this::handleTgMsgFailed);
-    } else {
-      Log.warn("Skipping unsupported event");
+    return processEvent(evt);
+  }
+
+  @Override
+  public BotSource getBotSource() {
+    return BotSource.TELEGRAM;
+  }
+
+  @Override
+  public Uni<Void> toggleTyping(String recipient, boolean isTyping) {
+    if (isTyping) {
+      return msgrApi.toggleTyping(recipient).replaceWithVoid();
     }
     return Uni.createFrom().voidItem();
   }
 
-  private Uni<Void> processLoadQuery(WebhookLoginDto login, RespDto<SessionDto> session, Update evt) {
-    Log.info("Authenticated: " + login.getUsername());
-    if (StringUtils.equals(session.getStatus(),
-      SessionDto.SessionStatus.ESTABLISHED.toString())) {
-      var query = new QueryDto();
-      query.setQuery(evt.message().text());
-      query.setBotSource(login.getBotSource());
-      return loadApi.query(query, "Bearer " + session.getResp().getAccessToken()).chain(resp -> {
-        Log.info("Query received, response is " + resp);
-        return sendMsgrMsg(evt, session.getResp().getUsername(),
-          "Working on your request, status is '%s'".formatted(resp.getStatus()));
-      });
-    }
-    return sendMsgrMsg(evt, session.getResp().getUsername(), session.getStatus());
+  @Override
+  public QueryDto createLoadQueryFromEvt(Update evt) {
+    var query = new QueryDto();
+    query.setQuery(evt.message().text());
+    query.setBotSource(getBotSource().toString());
+    return query;
   }
 
-  private void handleTgMsgFailed(Throwable sendMsgEx) {
-    Log.error("Telegram message sending failed: " + sendMsgEx.getMessage(), sendMsgEx);
-  }
-
-  private Uni<Void> handleApiEx(Update evt, Throwable ex) {
-    return handleApiEx(evt, null, ex);
-  }
-
-  private Uni<Void> handleApiEx(Update tgUpdate, String fpiUser, Throwable ex) {
-    Log.error("Error while processing evt: " + tgUpdate.updateId(), ex);
-    var apiSvcEx = (ApiSvcEx) ex;
-    Uni<Void> handleAction;
-
-    if (!HttpResponseStatus.INTERNAL_SERVER_ERROR.equals(apiSvcEx.getHttpResponseStatus())) {
-      var jsonResponse = apiSvcEx.getJsonResponse(RespDto.class);
-      if (jsonResponse != null && StringUtils.isNotBlank(jsonResponse.getError())) {
-        handleAction = sendMsgrMsg(tgUpdate, fpiUser, jsonResponse.getError());
-      } else {
-        handleAction = sendMsgrMsg(tgUpdate, fpiUser, "Error occurred, please try again");
-      }
-    } else {
-      handleAction = sendMsgrMsg(tgUpdate, fpiUser, "Error occurred, please try again");
-    }
-
-    return handleAction;
-  }
-
-  private Uni<Void> sendMsgrMsg(Update evt, String fpiUser, String msg) {
-    Log.info("Sending telegram msg: " + msg + " event: " + evt.updateId());
+  @Override
+  public MsgrMsgReqDto createMsgReqFromEvt(Update evt) {
     var msgReq = new MsgrMsgReqDto();
-    msgReq.setRecipient(String.valueOf(evt.message().chat().id()));
-    msgReq.setContent(msg);
+    msgReq.setRecipient(getSenderFromEvt(evt));
     msgReq.setReplyTo(String.valueOf(evt.message().messageId()));
-    return telegramApi.toggleTyping(msgReq.getRecipient())
-      .chain(() -> telegramApi.sendMsg(msgReq, fpiUser).replaceWithVoid());
+    return msgReq;
   }
 
-  private TelegramEvt mapToEntity(Update upd) {
+  @Override
+  public String getSenderFromEvt(Update evt) {
+    return String.valueOf(evt.message().from().id());
+  }
+
+  @Override
+  protected Uni<Void> sendMsgrMsg(MsgrMsgReqDto msgReq, String fpiUser) {
+    return msgrApi.sendMsg(msgReq, fpiUser).replaceWithVoid();
+  }
+
+  @Override
+  protected TelegramEvt mapToEntity(Update upd) {
     var entity = new TelegramEvt();
     entity.setSenderId(upd.message().from().id());
     entity.setUpdateId(upd.updateId());
@@ -163,6 +97,17 @@ public class TgMsgEvtPcsr {
     entity.setDateCreated(DateUtil.now());
     entity.setDateUpdated(DateUtil.now());
     return entity;
+  }
+
+  @Override
+  protected String getContentFromEvt(Update evt) {
+    return evt.message().text();
+  }
+
+  @Override
+  protected String getEventIdForLogging(Update evt) {
+    return "updateId: %d, senderId: %d, messageId: %d"
+      .formatted(evt.updateId(), evt.message().from().id(), evt.message().messageId());
   }
 
 }
